ff10fa46aacba98b67281e231de4e755d106663d
Eric Viana <ericvsdefault@gmail.com>
2022-11-27 18:38:32 -0300

    Initial commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,84 @@
+import './Main.scss'
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <table>
+    <tr>
+        <td id="left"><h2>Nome</h2></td>
+        <td id="center"><h2>Pra que serve</h2></td>
+        <td id="right"><h2>Como usar</h2></td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Advantam</h2></td>
+        <td> <p>Dermatites e dermatoses</p> </td>
+        <td> <p>Aplicar 1 camada fina nas regiões afetadas, 1 vez ao dia, durante 4 a 12 semanas.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Advantam Creme</h2></td>
+        <td> <p>Dermatites e dermatoses</p> </td>
+        <td> <p>Aplicar 1 camada fina nas regiões afetadas, 1 vez ao dia, durante 2 semanas.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Depo Medrol Predi-Medrol</h2></td>
+        <td> <p>Analgésico</p> </td>
+        <td> <p>Via injetável, posologia individualizada pra cada paciente.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Dipirona</h2></td>
+        <td> <p>Antipirético</p> </td>
+        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Fluoxetina</h2></td>
+        <td> <p>Anti-biótico</p> </td>
+        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Viagra</h2></td>
+        <td> <p>Anti-biótico</p> </td>
+        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Gelol</h2></td>
+        <td> <p>Anti-biótico</p> </td>
+        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Morfina</h2></td>
+        <td> <p>Anti-biótico</p> </td>
+        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Benegripe</h2></td>
+        <td> <p>Anti-biótico</p> </td>
+        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+    </tr>
+</table>
+    </div>
+  )
+}
+
+/*       <div className="main-container-title">
+        <h2>Nome</h2>
+        <h2>Categoria</h2>
+        <h2>Descrição</h2>
+      </div>
+      <div className="main-child">
+        <h2>Penicilina</h2>
+      </div>
+      <div className="main-child">
+        <h2>Poloramine</h2>
+      </div>
+      <div className="main-child">
+        <h2>Dipirona</h2>
+      </div>
+      <div className="main-child">
+        <h2>Paracetamol</h2>
+      </div>
+      <div className="main-child">
+        <h2>Fluoxetina</h2>
+      </div>
+      <div className="main-child">
+        <h2></h2>
+      </div> */
