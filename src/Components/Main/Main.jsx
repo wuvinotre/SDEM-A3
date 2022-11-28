bcc74379a6fc035451f910b976b8d1be12405926
vitorfogs <vitorhenrique.silva@hotmail.com>
2022-11-27 22:46:54 -0300

    vitor

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -21,38 +21,63 @@ export default function Main() {
     </tr>
     <tr>
         <td><h2 className=''>Depo Medrol Predi-Medrol</h2></td>
-        <td> <p>Analgésico</p> </td>
+        <td> <p>Doenças hematológicas</p> </td>
         <td> <p>Via injetável, posologia individualizada pra cada paciente.</p> </td>
     </tr>
     <tr>
-        <td><h2 className=''>Dipirona</h2></td>
-        <td> <p>Antipirético</p> </td>
-        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+        <td><h2 className=''>Unimedrol solupren</h2></td>
+        <td> <p>Corticoterapia</p> </td>
+        <td> <p>Frasco de 125mg e 500 mg Via intramuscular, posologia individualizada de cada paciente.</p> </td>
     </tr>
     <tr>
-        <td><h2 className=''>Fluoxetina</h2></td>
-        <td> <p>Anti-biótico</p> </td>
-        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+        <td><h2 className=''>Alerfin</h2></td>
+        <td> <p>Rinite</p> </td>
+        <td> <p>Via nasal, aplicar 2 jatos, em cada narina, 2 vezes ao dia</p> </td>
     </tr>
     <tr>
-        <td><h2 className=''>Viagra</h2></td>
-        <td> <p>Anti-biótico</p> </td>
-        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+        <td><h2 className=''>Clenil HFA</h2></td>
+        <td> <p>Asma brônquica</p> </td>
+        <td> <p>Via inalatória, usar 2 doses, 2 vezes ao dia</p> </td>
     </tr>
     <tr>
-        <td><h2 className=''>Gelol</h2></td>
-        <td> <p>Anti-biótico</p> </td>
-        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+        <td><h2 className=''>Blecort</h2></td>
+        <td> <p>Corticoterapia</p> </td>
+        <td> <p>Via inalatória, usar 2 doses, 2 vezes ao dia</p> </td>
     </tr>
     <tr>
-        <td><h2 className=''>Morfina</h2></td>
-        <td> <p>Anti-biótico</p> </td>
-        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+        <td><h2 className=''>Clenil Nasal Aquoso</h2></td>
+        <td> <p>Rinite</p> </td>
+        <td> <p>Via nasal, aplicar 1 a 2 doses, 2 vezes ao dia</p> </td>
     </tr>
     <tr>
-        <td><h2 className=''>Benegripe</h2></td>
-        <td> <p>Anti-biótico</p> </td>
-        <td> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p> </td>
+        <td><h2 className=''>Clenil Pulvinal</h2></td>
+        <td> <p>Asma brônquica</p> </td>
+        <td> <p>Via inalatória. Adultos: usar 1 dose, 2 a 4 vezes ao dia</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Clenil Pulvinal</h2></td>
+        <td> <p>Corticoterapia</p> </td>
+        <td> <p>Via inalatória. Crianças: usar 1 dose, 2 vezes ao dia</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Blecosol</h2></td>
+        <td> <p>Rinite</p> </td>
+        <td> <p>Via nasal, uso adulto e pediátrico a partir de 6  anos</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Fostair Aerosol</h2></td>
+        <td> <p>Asma brônquica</p> </td>
+        <td> <p>Via inalatória, usar 1 a 2 doses, 2 vezes ao dia</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Clenil Compositum HFA</h2></td>
+        <td> <p> Asma brônquica, doença pulmonar obstrutiva crônica</p> </td>
+        <td> <p>Via inalatória uso adulto e pediátrico a partir de  6 anos, Adultos: usae 2 doses, 4 ou 6 vezes ao dia, Crianças: usar 1 a 2 doses, 2 a 4 vezes ao dia</p> </td>
+    </tr>
+    <tr>
+        <td><h2 className=''>Areocort S</h2></td>
+        <td> <p> Asma brônquica, doença pulmonar obstrutiva crônica</p> </td>
+        <td> <p>Via inalatória uso adulto e pediátrico a partir de  6 anos, Adultos: usae 2 doses, 4 ou 6 vezes ao dia, Crianças: usar 1 a 2 doses, 2 a 4 vezes ao dia</p> </td>
     </tr>
 </table>
     </div>
